fd2398286eee15100a6e6b155d32d7e1914dca93
prasadg98 <145227919+prasadg98@users.noreply.github.com>
2025-11-11 20:20:02 -0500

    Time: 449 ms (88.27%) | Memory: 0B (100.00%) - LeetSync

--- 1623-sales-by-day-of-the-week/sales-by-day-of-the-week.sql
+++ 1623-sales-by-day-of-the-week/sales-by-day-of-the-week.sql
@@ -1,15 +1,19 @@
-SELECT
-  i.item_category AS Category,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 2 THEN o.quantity ELSE 0 END) AS `MONDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 3 THEN o.quantity ELSE 0 END) AS `TUESDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 4 THEN o.quantity ELSE 0 END) AS `WEDNESDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 5 THEN o.quantity ELSE 0 END) AS `THURSDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 6 THEN o.quantity ELSE 0 END) AS `FRIDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 7 THEN o.quantity ELSE 0 END) AS `SATURDAY`,
-  SUM(CASE WHEN DAYOFWEEK(o.order_date) = 1 THEN o.quantity ELSE 0 END) AS `SUNDAY`
-FROM Items i
-LEFT JOIN Orders o 
-  ON o.item_id = i.item_id
-GROUP BY i.item_category
-ORDER BY i.item_category;
-
+WITH CTE1 AS (
+    SELECT 
+        i.item_category AS CATEGORY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 2 THEN o.quantity ELSE 0 END) AS MONDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 3 THEN o.quantity ELSE 0 END) AS TUESDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 4 THEN o.quantity ELSE 0 END) AS WEDNESDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 5 THEN o.quantity ELSE 0 END) AS THURSDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 6 THEN o.quantity ELSE 0 END) AS FRIDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 7 THEN o.quantity ELSE 0 END) AS SATURDAY,
+        SUM(CASE WHEN DAYOFWEEK(o.order_date) = 1 THEN o.quantity ELSE 0 END) AS SUNDAY
+        
+    FROM Orders AS o
+    RIGHT JOIN Items AS i
+        ON o.item_id = i.item_id
+    GROUP BY i.item_category
+)
+SELECT * 
+FROM CTE1
+ORDER BY CATEGORY
